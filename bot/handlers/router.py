30f831429bcd4937fd5d1e5a927c29e16ca4a3f8
Kseniia Iarysh <kseniia.iarysh@orioninc.com>
2026-02-06 13:24:25 -0300

    fix

--- bot/handlers/router.py
+++ bot/handlers/router.py
@@ -8,18 +8,11 @@ from bot.handlers.astrology import router as astro_router
 from bot.handlers.mentoring import router as mentor_router
 from bot.handlers.payments import router as pay_router
 from bot.handlers.admin import router as admin_router
-from bot.handlers.yoga_feedback.handlers import router as yoga_feedback_router
 
 router = Router()
 
 
-# @router.callback_query()
-# async def _debug_all_callbacks(q: CallbackQuery):
-#     print("RAW CALLBACK DATA:", q.data)
-#     await q.answer()
-
 router.include_router(start_router)
-router.include_router(yoga_feedback_router)
 router.include_router(lang_router)
 router.include_router(yoga_router)
 router.include_router(astro_router)
